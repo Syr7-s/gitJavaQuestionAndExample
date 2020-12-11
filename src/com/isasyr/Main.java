e16aada640a404fe2553ed086b676c78fe47029c
Syr7-s <isasayar7@github.com>
2020-12-11 16:51:01 +0300

    numberAverage method added to Main.java

--- src/com/isasyr/Main.java
+++ src/com/isasyr/Main.java
@@ -1,7 +1,21 @@
 package com.isasyr;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    private Scanner scanner=new Scanner(System.in);
+    public static void main(String[] args) {
+        Main main=new Main();
+        System.out.println(main.numberAverage());
+    }
+    public double numberAverage(){
+        //Klavyeden girilen 3 sayının ortalaması.
+        System.out.print("First Number: ");
+        int firstNumber=scanner.nextInt();
+        System.out.print("Second Number: ");
+        int secondNumber=scanner.nextInt();
+        System.out.print("Third Number: ");
+        int thirdNumber=scanner.nextInt();
+        return (firstNumber+secondNumber+thirdNumber/3.0);
     }
 }
